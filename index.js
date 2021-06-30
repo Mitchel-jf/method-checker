4e8488d4713e529c13d8c1f62c2cb3f32286b87b
Mitchel-JF <mitchellcorporations@gmail.com>
2021-06-30 10:47:26 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http')
+require('dotenv').config()
+
+const { PORT } = process.env
+
+// this is the starting point of our method-checker
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.write(`Welcome to the Method-Checker API`)
+    res.write(`\nYou used ${req.method} to access this route`)
+    res.write('\nThank you')
+    return res.end()
+})
+
+server.listen(PORT, () =>
+    console.log(`Method-Checker started successfully on ${PORT}`))
